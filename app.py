b8e7930ef97c6d4ab8e10d475b6f039d1daa8a18
mohammadtahazaki1391-glitch <mohammadtahazaki1391@gmail.com>
2026-09-02 15:13:54 +0330

    Update app.py

--- app.py
+++ app.py
@@ -109,6 +109,5 @@ def buy_item(item_name):
     """
     return render_template_string(html, item=item, item_name=item_name)
 
-if name == "__main__":
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get('PORT', 10000))
+app.run(host='0.0.0.0', port=port)
